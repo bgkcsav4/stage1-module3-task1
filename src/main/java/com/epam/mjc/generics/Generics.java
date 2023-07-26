64a74de917889f0ddcc74545d0195cdf48369d77
Latsa <bgkcsavvy@gmail.com>
2023-07-26 09:41:25 +0200

    done

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -22,7 +22,7 @@ public class Generics {
 
 
     //TODO: Refactor Method-3
-    public <T> void cloneMethod(List<T> consumer, List<T> producer) {
+    public <T> void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 }
